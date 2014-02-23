13e9226e19f7d02af28cbf46d7d4a31505d857e7
cmcfarland <cmcfarland@cmcfarland.(none)>
2014-02-22 20:43:43 -0500

    Migrating project from old repository

--- test/services/GithubServiceITSpec.scala
+++ test/services/GithubServiceITSpec.scala
@@ -5,17 +5,14 @@ import play.api.test._
 import play.api.test.Helpers._
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import java.io.File
+import play.api.Play
 
 class GithubServiceITSpec extends Specification {
 
-  //  val app = FakeApplication(new File("/home/coleman/Code/righttrack/conf/application.conf"))
-
-
-//
+//  val app = FakeApplication(new File("/home/cmcfarland/Code/righttrack/conf/application.conf"))
 //  val config = Play.configuration(app)
-//  implicit val access_token = AccessToken(config.getString("github.oauth.token") getOrElse {
-//    throw new IllegalStateException("No github.oauth.token")
-//  })
+
 
   "Github integration" should {
 
